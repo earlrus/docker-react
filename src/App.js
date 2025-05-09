00eb7c8f299d94eebc7452db4beac4b4b353a71e
earlrus <basilcrouch5@gmail.com>
2025-05-09 11:13:59 +0000

    text changed

--- src/App.js
+++ src/App.js
@@ -7,15 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hi There!</p>
-        <p>Hello bro nice</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello bro nice to meet you</p>
       </header>
     </div>
   );
