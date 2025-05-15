e7ce21877c9d3f07a73e38325e08af27d4bbd908
Aman <basilcrouch10@gmail.com>
2025-05-15 14:06:48 +0530

    added changes for deployment apha test 2.0 copilot help

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>My pyara croco</h1>
+        <h1>My pyara croco and boat</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
